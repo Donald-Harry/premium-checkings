5d39a7ee25cdeeb8fa86aa1231ab11d531377562
Bright Munemu <brightmunemu@gmail.com>
2026-09-10 14:08:59 +0100

    add

--- backend/config.php
+++ backend/config.php
@@ -1,10 +1,10 @@
 <?php
-    $db_server = "localhost";
-    $db_username = "u755862803_premiumcheck";
-    $db_password = "Premium1234@!";
-    $db_name = "u755862803_premiumcheck";
+$db_server = "localhost";
+$db_username = "u840384314_premiumcheckin";
+$db_password = "Premiumcheckings1234@";
+$db_name = "u840384314_premiumcheckin";
 
-    $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-    if(!$conn){
-        echo "not connected". mysqli_connect_error();
-    }
+$conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+if (!$conn) {
+    echo "not connected" . mysqli_connect_error();
+}
